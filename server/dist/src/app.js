b4d6cd46dba2b0963ff6f678e1d194fa072eed9f
ikeshav26 <keshavgilhotra4@gmail.com>
2026-04-14 13:40:56 +0530

    Fixed some stuff

--- server/dist/src/app.js
+++
@@ -1,7 +0,0 @@
-import express, {} from 'express';
-const app = express();
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
-export default app;
-//# sourceMappingURL=app.js.map
